734f5e65de6a04df2841e3d3461dac32d769e505
asivathanu <asivathanu@enphaseenergy.com>
2018-09-29 00:30:13 +0530

    validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-Rails.application.routes.draw do
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Depot::Application.routes.draw do get "store/index"
+resources :products
+# ...
+# You can have the root of your site routed with "root"
+# just remember to delete public/index.html.
+# root :to => 'welcome#index'
+root to: 'store#index', as: 'store' # ...
 end
